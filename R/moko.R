3c277230b26175e035567dc63820505db78bb599
Adriano Passos <adriano.utfpr@gmail.com>
2016-09-17 03:09:13 +0000

    version 1.0.0

---
+++ R/moko.R
@@ -0,0 +1,11 @@
+#' moko: Multi-objective Kriging Optimization
+#'
+#' The package \code{moko} provides the user with methods for constrained and unconstraiend
+#' multi-objective optimization based on the popular Kriging surrogate model.
+#'
+#' The main functions provided by \code{moko} are:
+#' \code{\link{MEGO}}, \code{\link{HEGO}} and \code{\link{VMPF}}.
+#'
+#' @docType package
+#' @name moko
+NULL
